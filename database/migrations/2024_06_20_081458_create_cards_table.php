2d7731066ee551aa2c7c80641ad9f917b290f717
mehdi <mdaalvand@gmail.com>
2024-06-20 15:43:47 +0330

    Fix cards migration

--- database/migrations/2024_06_20_081458_create_cards_table.php
+++ database/migrations/2024_06_20_081458_create_cards_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('cards', function (Blueprint $table) {
             $table->id();
             $table->foreignId('account_id')->constrained()->onDelete('cascade');
-            $table->string('card_number')->unique();
+            $table->unsignedBigInteger('card_number')->unique();
             $table->decimal('balance');
             $table->timestamps();
         });
