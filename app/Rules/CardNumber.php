0a2a078171e8a23fd6e5deea446f7a2087ccfe90
mehdi <mdaalvand@gmail.com>
2024-06-20 15:45:32 +0330

    create validation rule for card number

---
+++ app/Rules/CardNumber.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class CardNumber implements ValidationRule
+{
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (!is_valid_card($value)) {
+            $fail(__('validation.invalid_card_number', $attribute));
+        }
+    }
+}
