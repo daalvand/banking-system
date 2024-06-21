e9ab91f6a094310aff9950dce6f4368b213c0f6f
mehdi <mdaalvand@gmail.com>
2024-06-21 15:15:52 +0330

    add main seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,21 @@
 
 namespace Database\Seeders;
 
+use App\Models\Account;
+use App\Models\Card;
+use App\Models\Fee;
+use App\Models\Transaction;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $users = User::factory()->count(10)->create();
+        foreach ($users as $user) {
+            $card = Card::factory()->for(Account::factory()->for($user))->create(['balance' => 10000]);
+            Transaction::factory(20)->has(Fee::factory())->create(['source_card_id' => $card->id]);
+        }
     }
 }
